1c537f766613dd9c0b00676b2bb6af787ab9ac36
Adi Wibawa <wibi.cms@gmail.com>
2018-12-10 18:57:38 +0700

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Question.create(content: '2 x 2 equals to ?', answer: 4)
+Question.create(content: 'The capital of Indonesia is ?', answer: 'Jakarta')
+Question.create(content: '3 + 3 equals to ?', answer: 6)
